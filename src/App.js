7d90cca2789d3b825ff02eac48139117cbd341e8
unknown <info.santanumaiti@gmail.com>
2020-07-23 00:38:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from STORE</h1>      
+    </div>
+  );
+}
+
+export default App;
